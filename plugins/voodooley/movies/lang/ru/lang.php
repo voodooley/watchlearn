d62a8ec082615d41998e1f9587cb1a75931ea2d0
Ruslan <voodooley@outlook.com>
2021-07-15 02:13:29 +0300

    Initial

---
+++ plugins/voodooley/movies/lang/ru/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Movies',
+        'description' => 'Учебный плагин'
+    ]
+];
---
+++ themes/watchlearn/assets/js/spritemap.js
@@ -0,0 +1,13 @@
+(self["webpackChunkoctobercms"] = self["webpackChunkoctobercms"] || []).push([["spritemap"],{
+
+/***/ "?4e0c":
+/*!******************************!*\
+  !*** spritemap-dummy-module ***!
+  \******************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
